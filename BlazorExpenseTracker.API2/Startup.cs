267c34e15c9c9e5726b71d936f411f506638352a
cruzhar <cruz_har@hotmail.es>
2022-09-04 23:25:53 -0600

    configuracion database

--- BlazorExpenseTracker.API2/Startup.cs
+++ BlazorExpenseTracker.API2/Startup.cs
@@ -1,3 +1,4 @@
+using BlazorExpenseTracker.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace BlazorExpenseTracker.API2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            var SqlConnectionConfiguration = new SqlConfiguration(Configuration.GetConnectionString("SqlConnection"));
+            services.AddSingleton(SqlConnectionConfiguration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
